11cbf812f9aad5ba9271fc73ee72b06de49f77d4
Chew Shee Yang <chewsheeyang01@gmail.com>
2025-10-16 15:01:27 +0800

    wip

--- tests/test_reflect.h
+++ tests/test_reflect.h
@@ -5,13 +5,51 @@
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/benchmark/catch_benchmark.hpp>
 
-TEST_CASE("Simple struct information", "[Reflect]")
+TEST_CASE("Simple struct reflection", "[reflection]")
 {
-    Simple s{42, 3.14f, 2.718};
+    Simple obj{42, 3.14f, 2.71};
 
-    REQUIRE(ReflectionInfo<Simple>::class_name == std::string("Simple"));
-    REQUIRE(Reflex::field_count<Simple> == 3);
-    REQUIRE(Reflex::get_field_count(s) == 3);
-    REQUIRE(Reflex::get_class_name<Simple>(s) == std::string("Simple"));
-    REQUIRE(Reflex::get_class_name(s) == std::string("Simple"));
+    SECTION("Class name")
+    {
+        REQUIRE(Reflex::class_name<Simple> == std::string("Simple"));
+        REQUIRE(Reflex::get_class_name(obj) == "Simple");
+    }
+
+    SECTION("Field count")
+    {
+        REQUIRE(Reflex::field_count<Simple> == 3);
+        REQUIRE(Reflex::get_field_count(obj) == 3);
+    }
+
+    SECTION("Set field value")
+    {
+        Reflex::set_field_value(obj, "integerValue", 100);
+        REQUIRE(obj.integerValue == 100);
+    }
+}
+
+TEST_CASE("AllPrimitiveTypes struct reflection", "[reflection]")
+{
+    AllPrimitiveTypes obj{
+        true, 'A', 123, 456, 789L, 101112LL,
+        'B', 234, 567, 890UL, 111213ULL,
+        1.23f, 4.56, 7.89L};
+
+    SECTION("Class name")
+    {
+        REQUIRE(Reflex::get_class_name(obj) == "AllPrimitiveTypes");
+    }
+
+    SECTION("Field count")
+    {
+        REQUIRE(Reflex::get_field_count(obj) == 14);
+    }
+
+    SECTION("Field iteration")
+    {
+        int count = 0;
+        Reflex::for_each_field(obj, [&](std::string_view name, auto &value, int)
+                               { count++; });
+        REQUIRE(count == 14);
+    }
 }
