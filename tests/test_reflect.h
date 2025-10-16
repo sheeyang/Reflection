6ad437adf8b26eb55dbde49e58404a3db556a126
Chew Shee Yang <chewsheeyang01@gmail.com>
2025-10-16 15:12:35 +0800

    check all values

--- tests/test_reflect.h
+++ tests/test_reflect.h
@@ -26,6 +26,13 @@ TEST_CASE("Simple struct reflection", "[reflection]")
         Reflex::set_field_value(obj, "integerValue", 100);
         REQUIRE(obj.integerValue == 100);
     }
+
+    SECTION("Field values")
+    {
+        REQUIRE(obj.integerValue == 42);
+        REQUIRE(obj.floatValue == 3.14f);
+        REQUIRE(obj.doubleValue == 2.71);
+    }
 }
 
 TEST_CASE("AllPrimitiveTypes struct reflection", "[reflection]")
@@ -52,6 +59,24 @@ TEST_CASE("AllPrimitiveTypes struct reflection", "[reflection]")
                                { count++; });
         REQUIRE(count == 14);
     }
+
+    SECTION("Field values")
+    {
+        REQUIRE(obj.booleanValue == true);
+        REQUIRE(obj.characterValue == 'A');
+        REQUIRE(obj.shortValue == 123);
+        REQUIRE(obj.integerValue == 456);
+        REQUIRE(obj.longValue == 789L);
+        REQUIRE(obj.longLongValue == 101112LL);
+        REQUIRE(obj.unsignedCharValue == 'B');
+        REQUIRE(obj.unsignedShortValue == 234);
+        REQUIRE(obj.unsignedIntValue == 567);
+        REQUIRE(obj.unsignedLongValue == 890UL);
+        REQUIRE(obj.unsignedLongLongValue == 111213ULL);
+        REQUIRE(obj.floatValue == 1.23f);
+        REQUIRE(obj.doubleValue == 4.56);
+        REQUIRE(obj.longDoubleValue == 7.89L);
+    }
 }
 
 TEST_CASE("Nested struct reflection", "[reflection]")
@@ -76,6 +101,28 @@ TEST_CASE("Nested struct reflection", "[reflection]")
         // Total fields: 3 (Nested) + 3 (Simple) + 14 (AllPrimitiveTypes) = 20
         REQUIRE(count == 20);
     }
+
+    SECTION("Field values")
+    {
+        REQUIRE(obj.count == 123);
+        REQUIRE(obj.simpleStruct.integerValue == 1);
+        REQUIRE(obj.simpleStruct.floatValue == 2.0f);
+        REQUIRE(obj.simpleStruct.doubleValue == 3.0);
+        REQUIRE(obj.primitiveTypes.booleanValue == true);
+        REQUIRE(obj.primitiveTypes.characterValue == 'C');
+        REQUIRE(obj.primitiveTypes.shortValue == 456);
+        REQUIRE(obj.primitiveTypes.integerValue == 789);
+        REQUIRE(obj.primitiveTypes.longValue == 101112L);
+        REQUIRE(obj.primitiveTypes.longLongValue == 131415LL);
+        REQUIRE(obj.primitiveTypes.unsignedCharValue == 200);
+        REQUIRE(obj.primitiveTypes.unsignedShortValue == 40000);
+        REQUIRE(obj.primitiveTypes.unsignedIntValue == 5000000);
+        REQUIRE(obj.primitiveTypes.unsignedLongValue == 60000000UL);
+        REQUIRE(obj.primitiveTypes.unsignedLongLongValue == 7000000000ULL);
+        REQUIRE(obj.primitiveTypes.floatValue == 2.34f);
+        REQUIRE(obj.primitiveTypes.doubleValue == 5.67);
+        REQUIRE(obj.primitiveTypes.longDoubleValue == 8.90L);
+    }
 }
 
 TEST_CASE("DeeplyNested struct reflection", "[reflection]")
@@ -99,6 +146,31 @@ TEST_CASE("DeeplyNested struct reflection", "[reflection]")
                                { count++; });
         // Total fields: 2 (DeeplyNested) + 3 (Nested) + 3 (Simple) + 14 (AllPrimitiveTypes) + 3 (Simple) = 25
         REQUIRE(count == 25);
+    }
+
+    SECTION("Field values")
+    {
+        REQUIRE(obj.nestedStruct.count == 456);
+        REQUIRE(obj.nestedStruct.simpleStruct.integerValue == 4);
+        REQUIRE(obj.nestedStruct.simpleStruct.floatValue == 5.0f);
+        REQUIRE(obj.nestedStruct.simpleStruct.doubleValue == 6.0);
+        REQUIRE(obj.nestedStruct.primitiveTypes.booleanValue == false);
+        REQUIRE(obj.nestedStruct.primitiveTypes.characterValue == 'D');
+        REQUIRE(obj.nestedStruct.primitiveTypes.shortValue == 789);
+        REQUIRE(obj.nestedStruct.primitiveTypes.integerValue == 101112);
+        REQUIRE(obj.nestedStruct.primitiveTypes.longValue == 131415L);
+        REQUIRE(obj.nestedStruct.primitiveTypes.longLongValue == 161718LL);
+        REQUIRE(obj.nestedStruct.primitiveTypes.unsignedCharValue == 200);
+        REQUIRE(obj.nestedStruct.primitiveTypes.unsignedShortValue == 50000);
+        REQUIRE(obj.nestedStruct.primitiveTypes.unsignedIntValue == 6000000);
+        REQUIRE(obj.nestedStruct.primitiveTypes.unsignedLongValue == 70000000UL);
+        REQUIRE(obj.nestedStruct.primitiveTypes.unsignedLongLongValue == 8000000000ULL);
+        REQUIRE(obj.nestedStruct.primitiveTypes.floatValue == 3.45f);
+        REQUIRE(obj.nestedStruct.primitiveTypes.doubleValue == 6.78);
+        REQUIRE(obj.nestedStruct.primitiveTypes.longDoubleValue == 9.01L);
+        REQUIRE(obj.simpleStruct.integerValue == 7);
+        REQUIRE(obj.simpleStruct.floatValue == 8.0f);
+        REQUIRE(obj.simpleStruct.doubleValue == 9.0);
     }
 }
 
@@ -129,5 +201,18 @@ TEST_CASE("ComplexTypes struct reflection", "[reflection]")
     {
         Reflex::set_field_value(obj, "stringValue", std::string("Goodbye, World."));
         REQUIRE(obj.stringValue == "Goodbye, World.");
+    }
+
+    SECTION("Field values")
+    {
+        REQUIRE(std::string(obj.cString) == "Hello, World!");
+        REQUIRE(obj.stringValue == "Reflect in C++");
+        REQUIRE(obj.doubleArray == std::array<double, 3>{1.1, 2.2, 3.3});
+        REQUIRE(obj.intVector == std::vector<int>{10, 20, 30, 40, 50});
+        REQUIRE(obj.stringFloatPair == std::pair<std::string, float>{"pi", 3.14f});
+        REQUIRE(obj.stringFloatMap == std::map<std::string, float>{{"pi", 3.14f}, {"e", 2.71f}});
+        REQUIRE(obj.intStringUnorderedMap == std::unordered_map<int, std::string>{{1, "one"}, {2, "two"}, {3, "three"}});
+        REQUIRE(obj.doubleList == std::list<double>{0.1, 0.2, 0.3, 0.4, 0.5});
+        REQUIRE(obj.intFloatStringTuple == std::tuple<int, float, std::string>{100, 200.0f, "three hundred"});
     }
 }
