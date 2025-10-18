dbe39dde883010ef1c6f1b2618cadd20eebd8a56
Chew Shee Yang <chewsheeyang01@gmail.com>
2025-10-19 05:00:29 +0800

    cleanup

--- include/ReflectionLibrary/Concepts.h
+++ include/ReflectionLibrary/Concepts.h
@@ -10,18 +10,24 @@ namespace ReflectionLibrary
   template <typename T>
   struct ReflectionInfo;
 
+  // Check if a type is reflectable
   template <typename T>
-  constexpr bool is_reflectable_v = requires { ReflectionInfo<T>::fields; };
+  concept is_reflectable_v = requires {
+    ReflectionInfo<T>::class_name;
+    ReflectionInfo<T>::fields;
+    ReflectionInfo<T>::reflect;
+    ReflectionInfo<T>::create;
+  };
 
-  // Helper concept to check if a type is a std::tuple
+  // Check if a type is a std::tuple
   template <typename T>
   concept is_tuple_v = requires { std::tuple_size<T>::value; };
 
-  // Helper concept to check if a type is a std::pair
+  // Check if a type is a std::pair
   template <typename T>
   concept is_pair_v = requires { typename T::first_type; typename T::second_type; };
 
-  // Helper concept to check if a type is iterable (has size, begin, end)
+  // Check if a type is iterable (has size, begin, end)
   template <typename T>
   concept is_iterable_v = requires(T value) { value.size(); value.begin(); value.end(); };
 
