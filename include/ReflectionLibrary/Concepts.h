153c17e4030f0c4bb0f0f0e6b82d49651106a571
Chew Shee Yang <chewsheeyang01@gmail.com>
2025-10-19 04:40:48 +0800

    wrap in ReflectionLibrary:: namespace

--- include/ReflectionLibrary/Concepts.h
+++ include/ReflectionLibrary/Concepts.h
@@ -4,28 +4,31 @@
 #include <concepts>
 #include <array>
 
-// Forward declaration
-template <typename T>
-struct ReflectionInfo;
+namespace ReflectionLibrary
+{
+  // Forward declaration
+  template <typename T>
+  struct ReflectionInfo;
 
-template <typename T>
-constexpr bool is_reflectable_v = requires { ReflectionInfo<T>::fields; };
+  template <typename T>
+  constexpr bool is_reflectable_v = requires { ReflectionInfo<T>::fields; };
 
-// Helper concept to check if a type is a std::tuple
-template <typename T>
-concept is_tuple_v = requires { std::tuple_size<T>::value; };
+  // Helper concept to check if a type is a std::tuple
+  template <typename T>
+  concept is_tuple_v = requires { std::tuple_size<T>::value; };
 
-// Helper concept to check if a type is a std::pair
-template <typename T>
-concept is_pair_v = requires { typename T::first_type; typename T::second_type; };
+  // Helper concept to check if a type is a std::pair
+  template <typename T>
+  concept is_pair_v = requires { typename T::first_type; typename T::second_type; };
 
-// Helper concept to check if a type is iterable (has size, begin, end)
-template <typename T>
-concept is_iterable_v = requires(T value) { value.size(); value.begin(); value.end(); };
+  // Helper concept to check if a type is iterable (has size, begin, end)
+  template <typename T>
+  concept is_iterable_v = requires(T value) { value.size(); value.begin(); value.end(); };
 
-template <typename T>
-concept has_reflector_v = requires(const T &obj) {
+  template <typename T>
+  concept has_reflector_v = requires(const T &obj) {
     typename T::Reflector;
     { T::Reflector::reflect(obj) } -> std::same_as<typename T::Reflector>;
     { T::Reflector::create(std::declval<typename T::Reflector>()) } -> std::same_as<T>;
-};
+  };
+}
